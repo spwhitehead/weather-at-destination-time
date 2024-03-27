228f136a0e87d2f91d4b0e3b5724a77e31551be0
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-27 10:17:05 -0600

    Adds geocode.py and sets it up

---
+++ geocode.py
@@ -0,0 +1,19 @@
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderUnavailable
+
+# Initialize Nominatim Geocoder with a specific user agent and a longer timeout
+geolocator = Nominatim(user_agent="wx_destination_app_v1",
+                       timeout=5)  # Timeout set to 5 seconds
+
+
+def get_geocoded_address(address):
+    try:
+        # Perform geocoding to get latitude and longitude
+        location = geolocator.geocode(address)
+
+        if location:
+            return location.latitude, location.longitude
+        else:
+            print("Location could not be geocoded")
+    except GeocoderUnavailable:
+        print("Geocoder service is unavailable. Please try again later.")
