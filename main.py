8d39e09ab9bc40e2aadaa20a80aec481c7ea97c5
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-27 11:44:51 -0600

    Updates main.py and index.html for fastAPI

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 import geocode
 import weather
 
-from fastapi import FastAPI
-
 
 app = FastAPI
+templates = Jinja2Templates(directory="templates")
 
+
+@app.get("/", response_class=HTMLResponse)
+async def get_form(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/weather-at-location-and-time/", response_class=HTMLResponse)
+async def submit_form(request: Request, address: str = Form(...), time: int = Form(...)):
+    lat, lon = geocode.get_geocoded_address(address)
+    weather_data = weather.fetch_and_display_wx_for_time(lat, lon, time)
+    return templates.TemplateResponse("index.html", {"request": request, "message": weather_data})
+
+
+"""
 # Get Destination Location
-
-
 def get_location():
     address = input("Enter the desired address or city: ")
     return geocode.get_geocoded_address(address)
@@ -31,3 +46,4 @@ def main():
     destination_forecast = weather.fetch_and_display_wx_for_time(
         latitude, longitude, hours)
     return (f"Weather at desired location and time: {destination_forecast}")
+"""
