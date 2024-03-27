c6d8e76301d7154e3f5c97a78154c8e404ce2819
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-27 10:43:53 -0600

    Adds input for desired address

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import geocode
+import weather
+
 from fastapi import FastAPI
 
+
 app = FastAPI
+
+# Get Destination Location
+
+
+def get_location():
+    address = input("Enter the desired address or city: ")
+    return geocode.get_geocoded_address(address)
